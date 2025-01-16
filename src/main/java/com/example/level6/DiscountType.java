6da797ca2ed9f01602acd1143281db6eb06ae037
jyedev04 <jyedev04@gmail.com>
2025-01-16 20:19:21 +0900

    키오스크 Lv6 생성  - Order.java 수량 추가  - order list->  Map으로 수정  - enum을 이용하여 할인 기능 추가

---
+++ src/main/java/com/example/level6/DiscountType.java
@@ -0,0 +1,37 @@
+package com.example.level6;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public enum DiscountType {
+    NATIONAL_MERIT( "국가유공자", 0.1),
+    ARMY( "군인", 0.05),
+    STUDENT("학생", 0.03),
+    ORDINARY("일반", 0);
+
+    private final String type;
+    private final double discount;
+
+    DiscountType(String type, double discount) {
+        this.type = type;
+        this.discount = discount;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public double getDiscount() {
+        return discount;
+    }
+
+    @Override
+    public String toString() {
+        return type + ": " + (int)(discount*100) +"%";
+    }
+
+    // 할인율 체크
+    public static double chkDiscount(int index) {
+        return DiscountType.values()[index-1].getDiscount();
+    }
+}
